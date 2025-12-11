ab68b98edd5385ad0eb9dfde7f83a70b2a850bb8
Quanda666 <164914855+Quanda666@users.noreply.github.com>
2025-12-12 02:51:36 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,24 +339,22 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "默认后端": "http://sub.xxvxx.ggff.net",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           nameless13提供: "https://www.nameless13.com",
           subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
+          "本站提供": "https://sub.xxvxx.ggff.net",
         },
         backendOptions: [
-          { value: "http://sub.xxvxx.ggff.net" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
           { value: "https://sub.d1.mk" },
           { value: "https://www.nameless13.com" },
           { value: "https://sub.xeton.dev" },
-          { value: "https://api.wcc.best" },
+          { value: "https://sub.xxvxx.ggff.net" },
         ],
         remoteConfig: [
           {
@@ -1313,3 +1311,4 @@ export default {
   }
 };
 </script>
+
